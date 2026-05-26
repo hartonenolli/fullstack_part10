4ef021444bbfd29ebe780a7e773fc964178ef36e
hartonenolli <hartonenolli@gmail.com>
2026-05-26 11:06:52 +0300

    1. initializing the application

---
+++ rate-repository-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Small change is all you need!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
