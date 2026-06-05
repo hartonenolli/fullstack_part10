b7ff838d055b2b3d5e2906d08b6941e4b6099a8a
hartonenolli <hartonenolli@gmail.com>
2026-06-05 09:55:40 +0300

    4. The app bar

---
+++ rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,27 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: theme.colors.appBarBackground,
+    display: 'flex',
+    paddingLeft: 10,
+    paddingBottom: 20,
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable>
+        <Text color="textAppBar" fontWeight="bold" fontSize="subheading">Repositories</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default AppBar;
