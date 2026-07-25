279b938c6d88603b037f12a5044acf39bdc7d103
hartonenolli <hartonenolli@gmail.com>
2026-07-25 18:12:37 +0300

    11. Fetching repositories with Apollo Client

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
-import useRepositories from '../hooks/useRepositories';
+import { useQuery } from '@apollo/client/react';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+
 import RepositoryItem from './RepositoryItem';
 import { FlatList, View, StyleSheet } from 'react-native';
 
@@ -11,15 +14,22 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
  
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const { loading, error, data } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+  // console.log({ loading, error, data });
+
+  const repositoryNodes = data && data.repositories
+    ? data.repositories.edges.map(edge => edge.node)
     : [];
+  
+// console.log('repositoryNodes:', repositoryNodes);
 
   return (
     <FlatList
       data={repositoryNodes}
+      keyExtractor={(item) => item.id}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem repository={item} />}
     />
